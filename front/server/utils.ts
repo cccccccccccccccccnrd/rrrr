f4194cfcb17c087adfce4283da7f4f0a9363a51a
cccccccccccccccccnrd <hello@conradweise.com>
2023-11-10 23:16:27 +0100

    eco

--- front/server/utils.ts
+++ front/server/utils.ts
@@ -1,11 +1,13 @@
 import fetch from 'node-fetch'
 
 export async function request (path: string) {
+  console.log(useRuntimeConfig().baseURL, useRuntimeConfig().auth)
   const response = await fetch(`${useRuntimeConfig().baseURL}/api/${path}`, {
     headers: {
       Authorization: `${useRuntimeConfig().auth}`
     }
   })
+  console.log(response)
   const json: any = await response.json()
   console.log(json.data)
   return json.data
